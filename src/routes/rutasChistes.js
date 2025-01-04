3f1fc83b58fe93b4ecfe3e1d159675712c9f1598
morochobrandon <bggutierrez.21@est.ucab.edu.ve>
2025-01-04 15:42:23 -0400

    segundo endpoint que nos dijo que hicieramos la profesora

--- src/routes/rutasChistes.js
+++ src/routes/rutasChistes.js
@@ -39,4 +39,32 @@ router.get("/", async (req, res) => {
   }
 });
 
+//POST: Para guardar un chiste en la base de datos:
+
+const categoriasPermitidas = ['Dad joke', 'Humor Negro', 'Chistoso', 'Malo'];
+
+router.post('/', async (req, res) => {
+    try {
+        const { texto, autor = 'Se perdió en el Ávila como Led', puntaje, categoria } = req.body;
+
+
+
+        if (!texto || !puntaje || !categoria) {
+            return res.status(400).send({ error: 'Texto, puntaje y categoría son requeridos' });
+        }        
+        if (!categoriasPermitidas.includes(categoria)){
+          return res.status(400).send({ error: 'Categoría no válida. Las categorías permitidas son: Dad joke, Humor Negro, Chistoso, Malo' });
+        }
+
+
+        const nuevoChiste = new Chiste({ texto, autor, puntaje, categoria });
+        await nuevoChiste.save();
+
+        return res.status(201).send({ id: nuevoChiste._id });
+    } catch (err) {
+        return res.status(500).send({ error: 'Error al guardar el chiste' });
+    }
+});
+
+
 export default router;
